f4437aaec3c601874d3edc2773d60634e9bd2e3b
saiyamjain0 <saiyamjain246@gmail.com>
2026-03-17 16:54:49 +0530

    Update SIEVE_ERATOSTHANES.cpp

--- NUMBER THEORY/SIEVE_ERATOSTHANES.cpp
+++ NUMBER THEORY/SIEVE_ERATOSTHANES.cpp
@@ -1,14 +1,14 @@
 // IF sieve[k] = 0 then k is prime else sieve[k] is highest prime factor of k.
 for(int x = 2 ; x <= n; ++n){
   if (sieve[x]) continue;
-  for(int u = 2*x; u<= n; u+=s){
+  for(int u = x; u<= n; u+=s){
     sieve[x] = u;
   }
 }
 // IF sieve[k] = 0 then k is prime else sieve[k] is lowest prime factor of k.
 for(int x = 2 ; x <= n; ++n){
   if (sieve[x]) continue;
-  for(int u = 2*x; u<= n; u+=s){
+  for(int u = x; u<= n; u+=s){
     if (sieve[u]) continue;
     sieve[x] = u;
   }
